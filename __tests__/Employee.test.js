a061ce7410a4d2fd221639e6857e851be06db0e1
Shannon <shannon.nell92@gmail.com>
2021-06-20 14:32:40 -0400

    5 employee tests pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -10,3 +10,27 @@ test('creates employee object', () => {
     expect (employee.id).toEqual(expect.any(Number));
     expect (employee.role).toBe('Employee');
 });
+
+//return name
+test('returns employee name', () => {
+    const employee = new Employee ('Kaladin', 'bridgefour@shatteredplains.com');
+    expect(employee.getName()).toBe('Kaladin');
+});
+
+//return employee id
+test('returns employee ID number', () => {
+    const employee = new Employee ('Kaladin', 'bridgefour@shatteredplains.com');
+    expect(employee.getID()).toEqual(expect.any(Number));
+});
+
+//return employee email
+test('returns employee email', () => {
+    const employee = new Employee ('Kaladin', 'bridgefour@shatteredplains.com');
+    expect(employee.getEmail()).toBe('bridgefour@shatteredplains.com');
+});
+
+//return employee role
+test('returns employee role', () => {
+    const employee = new Employee ('Kaladin', 'bridgefour@shatteredplains.com');
+    expect(employee.getRole()).toBe('Employee');
+});
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,6 @@
+const Manager = require('../lib/Manager');
+
+//creates Manager
+test('creates manager object', () => {
+    const manager = new Manager ('Dalinar')
+});
