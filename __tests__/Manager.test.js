997c4ad0df9dd3d07f0079a2d2126e649e6819cb
Shannon <shannon.nell92@gmail.com>
2021-06-20 14:40:55 -0400

    Initial manager tobj created and passed

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,5 +2,9 @@ const Manager = require('../lib/Manager');
 
 //creates Manager
 test('creates manager object', () => {
-    const manager = new Manager ('Dalinar')
+    const manager = new Manager ('Dalinar', 'dkholin@uruthiru.com', '111-111-1111');
+
+    expect(manager.name).toBe('Dalinar');
+    expect(manager.email).toBe('dkholin@uruthiru.com');
+    expect(manager.officeNumber).toBe('111-111-1111');
 });
