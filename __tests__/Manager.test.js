45ad793c067b64a8bc12629f16047d9b3b3112a5
Shannon <shannon.nell92@gmail.com>
2021-06-20 14:49:47 -0400

    Manager officeNum & role updated and pass

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -8,3 +8,18 @@ test('creates manager object', () => {
     expect(manager.email).toBe('dkholin@uruthiru.com');
     expect(manager.officeNumber).toBe('111-111-1111');
 });
+
+//test for office number
+test('returns office number', () => {
+    const manager = new Manager ('Dalinar', 'dkholin@uruthiru.com', '111-111-1111');
+    
+    expect(manager.getOfficeNumber()).toBe('111-111-1111');
+});
+
+//test override role
+test('overrides employee role', () => {
+    const manager = new Manager ('Dalinar', 'dkholin@uruthiru.com', '111-111-1111');
+
+    expect(manager.role).toBe('Manager');
+    expect(manager.getRole()).toBe('Manager');
+});
