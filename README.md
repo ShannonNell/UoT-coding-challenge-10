0950424423ce2d638fb6d38874aca5fec9a03a59
Shannon <shannon.nell92@gmail.com>
2021-06-23 14:30:42 -0400

    Manager inquirer generated

---
+++ README.md
@@ -0,0 +1,78 @@
+# Challenge 10 OOP: Team Profile Generator
+
+## Description
+A Node.js command-line app that takes in information about employees on a software engineering team and generates an HTML webpage that displays summaries for each person.
+
+Done as a challenge for UoT's Coding Bootcamp.
+___
+
+## Talbe of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Links](#links)
+* [Tools](#tools)
+* [Credits](#credits)
+* [License](#license)
+* [Challenge Guidelines](#challenge-guidelines)
+___
+
+## Installation
+
+___
+
+## Usage
+Application invoked by using: `node index.js`
+
+![screenshotHere](/assets/images/)
+___
+
+## Links
+### Walkthrough of app:
+* [Video Walkthrough]()
+___
+
+## Tools
+* node.js
+* JavaScript
+* Jest - for running tests
+* Inquirer - for collecgint input from user
+___
+
+## Credits
+* Completed by: [Nell-GitHub](https://github.com/ShannonNell)
+___
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+[MIT License](https://choosealicense.com/licenses/mit/)    
+___
+
+## Challenge Guidelines
+### User Story
+```
+AS A manager
+I WANT to generate a webpage that displays my team's basic info
+SO THAT I have quick access to their emails and GitHub profiles
+```
+
+### Criteria: 
+```
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for my team members and their information
+THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+WHEN I click on an email address in the HTML
+THEN my default email program opens and populates the TO field of the email with the address
+WHEN I click on the GitHub username
+THEN that GitHub profile opens in a new tab
+WHEN I start the application
+THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+WHEN I enter the team manager’s name, employee ID, email address, and office number
+THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+WHEN I select the engineer option
+THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+WHEN I select the intern option
+THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+WHEN I decide to finish building my team
+THEN I exit the application, and the HTML is generated
+```
--- index.js
+++ index.js
@@ -1,28 +1,65 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-//prompr user for answers to questions
-const promptUser = () => {
-    //prompt user for employee information
-    inquirer
-        .prompt([
-            { //name
-                type: 'input',
-                name: 'EmployeeName',
-                message: "What is your employee's name?",
-            },
-            // { //id
-            //     type: 'list',
-            //     name: 'EmployeeID',
-            //     message: "What is your employee's ID? Enter 1 for Manager, 2 for Engineer, 3 for Intern.",
-            //     choices: ['1', '2', '3'],
-            // },
-            { //email
-                type: 'input',
-                name: 'EmployeeEmail',
-                message: "What is your employee's email?",
-            }
-        ]);
+// require manager, engineer, intern
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+// require generateHTML
+// require writeFile
+
+let dataArr = [];
+
+//getManagerInfo inquirer
+const getManagerInfo = () => {
+    return inquirer
+    .prompt([
+        { //manager name
+            type: 'input',
+            name: 'managerName',
+            message: "Welcome to the Team Profile Generator. What is your team manager's name?",
+        },
+        { //manager ID
+            type: 'input',
+            name: 'managerId',
+            message: "What is your team manager's employee ID number?",
+        },
+        { //manager email
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is your team manager's email?",
+        },
+        { //manager office number
+            type: 'input',
+            name: 'managerOfficeNum',
+            message: "What is your team manager's office phone number?",
+        },
+    ])
+    .then((data) => {
+        const manager = new Manager(
+            data.managerName,
+            data.managerEmail,
+            data.managerOfficeNum
+        );
+        dataArr.push(manager);
+        console.log(dataArr);
+    })
+    // .then(addToTeam); //this function needs to be created/adjusted still
 };
 
-promptUser();
+//getEngineerInfo inquirer
+
+//getInternInfo inquirer
+
+//choices to add another team member inquirer
+
+//start with getManagerInfo
+getManagerInfo();
+
+module.exports = dataArr;
+
+
+
+//TODO:
+//validation
+//ID number organized
