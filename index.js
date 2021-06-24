889285b3c9e643983c48f9f44fe5e6095692af43
Shannon <shannon.nell92@gmail.com>
2021-06-24 09:11:31 -0400

    getEngineer, get Intern inquirer and moved to buildTeam.js

--- index.js
+++ index.js
@@ -1,89 +1,9 @@
-const inquirer = require('inquirer');
-// const fs = require('fs');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
 // require generateHTML
 // require writeFile
-
-function Team() {
-    this.manager;
-    this.engineer;
-    this.intern;
-};
-
-//getManagerInfo inquirer
-Team.prototype.getManagerInfo = function () {
-    inquirer.prompt([
-        { //manager name
-            type: 'input',
-            name: 'name',
-            message: "Welcome to the Team Profile Generator. What is your team manager's name?",
-        },
-        { //manager ID
-            type: 'input',
-            name: 'id',
-            message: "What is your team manager's employee ID number?",
-        },
-        { //manager email
-            type: 'input',
-            name: 'email',
-            message: "What is your team manager's email?",
-        },
-        { //manager office number
-            type: 'input',
-            name: 'officeNum',
-            message: "What is your team manager's office phone number?",
-        },
-    ])
-        .then(({ name, id, email, officeNum }) => {
-            this.manager = new Manager(name);
-            this.buildTeam(); 
-        })
-};
-
-//build the team inquirer
-Team.prototype.buildTeam = function () {
-    inquirer.prompt([
-        { //what team member to add
-            type: 'list',
-            name: 'buildTeam',
-            message: 'What would you like to do next?',
-            choices: ["Add an engineer", "Add an intern", "I'm finished building my team."]
-        }
-    ])
-        .then(({ buildTeam }) => {
-            if (buildTeam === 'Add an engineer') {
-                this.getEngineerInfo();
-            }
-            if (buildTeam === 'Add an intern') {
-                this.getInternInfo();
-            }
-            if (buildTeam === "I'm finished building my team.") {
-                console.log("I'm finished building my team");
-                //write data to file or return false?
-            }
-        })
-};
+// require fs
+const Team = require('./lib/BuildTeam');
 
 
-//getEngineerInfo inquirer
-Team.prototype.getEngineerInfo = function () {
-    console.log("I'm an engineer");
-}
-
-//getInternInfo inquirer
-Team.prototype.getInternInfo = function () {
-    console.log("I'm an intern!");
-}
-
-//choices to add another team member inquirer
-
-
-//start with getManagerInfo
-
-
-// module.exports = Team;
 new Team().getManagerInfo();
 
 
