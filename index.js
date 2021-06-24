fd896e097324707df50ba6c3ce2c85217c9a419f
Shannon <shannon.nell92@gmail.com>
2021-06-24 08:43:51 -0400

    Build Team function created

--- index.js
+++ index.js
@@ -38,23 +38,58 @@ Team.prototype.getManagerInfo = function () {
     ])
         .then(({ name, id, email, officeNum }) => {
             this.manager = new Manager(name);
-            //this.buildTeam(); //this function needs to be created/adjusted still
+            this.buildTeam(); 
         })
 };
 
+//build the team inquirer
+Team.prototype.buildTeam = function () {
+    inquirer.prompt([
+        { //what team member to add
+            type: 'list',
+            name: 'buildTeam',
+            message: 'What would you like to do next?',
+            choices: ["Add an engineer", "Add an intern", "I'm finished building my team."]
+        }
+    ])
+        .then(({ buildTeam }) => {
+            if (buildTeam === 'Add an engineer') {
+                this.getEngineerInfo();
+            }
+            if (buildTeam === 'Add an intern') {
+                this.getInternInfo();
+            }
+            if (buildTeam === "I'm finished building my team.") {
+                console.log("I'm finished building my team");
+                //write data to file or return false?
+            }
+        })
+};
+
+
 //getEngineerInfo inquirer
+Team.prototype.getEngineerInfo = function () {
+    console.log("I'm an engineer");
+}
 
 //getInternInfo inquirer
+Team.prototype.getInternInfo = function () {
+    console.log("I'm an intern!");
+}
 
 //choices to add another team member inquirer
+
 
 //start with getManagerInfo
 
 
-module.exports = Team;
+// module.exports = Team;
+new Team().getManagerInfo();
 
 
 
 //TODO:
 //validation
 //ID number organized
+//move team to own js file in lib?
+//phone number validation as 111-111-1111 || 1111111111 \\ 111 111 1111
